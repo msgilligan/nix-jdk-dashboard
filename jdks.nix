db21582b12a8cbb0b29723d5f514f5a807001cd2
Sean Gilligan <sean@msgilligan.com>
2025-09-19 10:57:36 -0700

    Add openjdk8

--- jdks.nix
+++ jdks.nix
@@ -3,7 +3,7 @@
     corretto = { hasDefault = false; separator = ""; versions = [ "11" "17" "21" ]; };
     graalvm-ce = { hasDefault = true; separator = "_"; path="graalvmPackages.graalvm-ce"; versions = []; };
     graalvm-oracle = { hasDefault = true; separator = "_"; path="graalvmPackages.graalvm-oracle"; versions = [ "17" ]; };
-    openjdk = { hasDefault = true; separator = ""; versions = [ "11" "17" "21" "24" ]; };
+    openjdk = { hasDefault = true; separator = ""; versions = [ "8" "11" "17" "21" "24" ]; };
     semeru-bin = { hasDefault = true; separator = "-"; versions = [ "11" "17" "21" ]; };
     semeru-jre-bin = { hasDefault = true; separator = "-"; versions = [ "11" "17" "21" ]; };
     temurin-bin = { hasDefault = true; separator = "-"; versions = [ "23" "24" ]; };
